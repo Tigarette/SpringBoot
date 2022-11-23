f5399e8bccb15554c7000a47cc0391b48b33b4d9
谭 杨艺 <2054806568@qq.com>
2022-11-23 10:43:14 +0800

    梅开二改

--- backend/src/main/java/com/hnieacm/media/model/Secret.java
+++ backend/src/main/java/com/hnieacm/media/model/Secret.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @AutoConfigureBefore(ConnectTencent.class)
 @Configuration
